1535aeaf7eb869c1226b12db2b41d12980e8c306
sivaezhumalai <19130331@velsuniv.ac.in>
2025-02-16 19:58:33 +0530

    add Tailwind CSS configuration and update contact form styling

---
+++ css/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["../**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ["Pacifico", "cursive"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      height: {
+        "screen-90": "90vh",
+      },
+    },
+  },
+  plugins: [],
+};
